aebd7d9237914071d034d49f2e136e2b7f1dc3a4
A-Schemitt <91761374+A-Schemitt@users.noreply.github.com>
2022-03-26 23:53:35 -0300

    'conserto' do app, criação da minha classe

--- App.java
+++ App.java
@@ -20,8 +20,9 @@ public class App {
         System.out.println("----------------------------------");
     }
 
-    public static void main(String args[]) {
-        Path filesPath = Paths.get("..", "Codificadores");
+    public static void main(String args[]) throws IOException {//estou no windows e
+        String currentPath = new java.io.File(".").getCanonicalPath();//não estava conseguindo rodar o programa, então
+        Path filesPath = Paths.get(currentPath, "Codificadores");//precisei mudar essas 3 linhas pra fazer funcionar
         System.out.println("Pasta:"+filesPath.getFileName());
         List<String> codificadores = null;
 
---
+++ Codificadores/Codifica20204032.java
@@ -0,0 +1,29 @@
+package Codificadores;
+
+public class Codifica20204032 implements Codifica {
+
+    @Override
+    public String getNomeAutor() {
+        return "Alison G. Schemitt";
+    }
+
+    @Override
+    public String getMatriculaAutor() {
+        return "20204032-5";
+    }
+
+    @Override
+    public String codifica(String str) {
+        
+
+        return str;
+    }
+
+    @Override
+    public String decodifica(String str) {
+        
+        
+        return str;
+    }
+    
+}
